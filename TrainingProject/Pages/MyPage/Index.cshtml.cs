b3dc4d5653312bcbafb586373d6db33edd0be9ee
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-05-04 13:11:32 +0200

    add Account data to db

--- TrainingProject/Pages/MyPage/Index.cshtml.cs
+++ TrainingProject/Pages/MyPage/Index.cshtml.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TrainingProject.Data;
+using TrainingProject.Models;
 
 namespace TrainingProject.Pages.MyPage
 {
-    public class IndexModel : PageModel
-    {
-        public int loggedInID { get; set; }
+	public class IndexModel : PageModel
+	{
+		public int loggedInID { get; set; }
 
 		private readonly AppDbContext context;
 		public IndexModel(AppDbContext context, AccessControl access)
@@ -15,12 +16,23 @@ namespace TrainingProject.Pages.MyPage
 			loggedInID = access.LoggedInAccountID;
 		}
 		public void OnGet()
-        {
-        }
+		{
+		}
 
-        public IActionResult OnPut(int age, int height, int weight, bool gender, int targetWeight, DateOnly tagetDate)
-        {
-            return NotFound();
-        }
-    }
+		public IActionResult OnPost(int age, int height, int weight, bool gender, int targetWeight, DateTime targetDate)
+		{
+			var user = context.Accounts.First(u => u.Id == loggedInID);
+
+			user.Age = age;
+			user.Height = height;
+			user.CurentWeight = weight;
+			user.IsMale = gender;
+			user.TargetWeight = targetWeight;
+			user.TargetDate = targetDate;
+
+			context.SaveChanges();
+
+			return Page();
+		}
+	}
 }
