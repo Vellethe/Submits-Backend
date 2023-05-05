2a51b990691a74d45c81493baa0482d26549e5c3
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-05-05 10:19:38 +0200

    model has enum now and edit changes

--- TrainingProject/Pages/Workout/Edit.cshtml.cs
+++ TrainingProject/Pages/Workout/Edit.cshtml.cs
@@ -27,6 +27,7 @@ namespace TrainingProject.Pages.Workout
                 SelectedWorkout = new Models.Workout { Owner = context.Accounts.First(), Id = 0, Name = "test" };
                 context.Workouts.Add(SelectedWorkout);
                 context.SaveChanges();
+                //dose not work if user navigates directly to workout/edit/
                 return Redirect($"./Edit/{SelectedWorkout.Id}");
             }
             else
@@ -53,12 +54,42 @@ namespace TrainingProject.Pages.Workout
             SelectedWorkout.WorkoutExecises.Add(new WorkoutExecise
             {
                 Workout = SelectedWorkout,
-                Intensity = intesnity,
+                Intensity = (InetensityLevel)intesnity,
                 Exercise = context.Exercises.Find(exersieId)
             });
             context.SaveChanges();
 
 
+            return Redirect($"./{SelectedWorkout.Id}");
+        }
+
+        public IActionResult OnPostDelete(int id, int exersieId)
+        {
+            Id = id;
+            SelectedWorkout = context.Workouts.Include(x => x.WorkoutExecises).ThenInclude(x => x.Exercise).Where(x => Id == x.Id).FirstOrDefault();
+            if (SelectedWorkout == null)
+            {
+                return NotFound();
+            }
+            SelectedWorkout.WorkoutExecises.Remove(context.WorkoutExecises.Find(exersieId));
+
+            context.SaveChanges();
+
+            return Redirect($"./{SelectedWorkout.Id}");
+        }
+
+        public IActionResult OnPostChange(int id, int intesnity, int workoutExerciseId)
+        {
+
+            Id = id;
+            SelectedWorkout = context.Workouts.Include(x => x.WorkoutExecises).ThenInclude(x => x.Exercise).Where(x => Id == x.Id).FirstOrDefault();
+            if (SelectedWorkout == null)
+            {
+                return NotFound();
+            }
+
+            SelectedWorkout.WorkoutExecises.First(x=>x.Id == workoutExerciseId).Intensity = (InetensityLevel)intesnity;
+            context.SaveChanges();
             return Redirect($"./{SelectedWorkout.Id}");
         }
     }
