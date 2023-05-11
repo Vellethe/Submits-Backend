dd1fef20e16b5a83d8e8cc0f2b469b24dbe92ed7
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-05-11 10:55:42 +0200

    HomePage

--- TrainingProject/Pages/Index.cshtml.cs
+++ TrainingProject/Pages/Index.cshtml.cs
@@ -1,21 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using TrainingProject.Data;
+using TrainingProject.Models;
 
 namespace TrainingProject.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly AppDbContext database;
+        public string RandomMotivation { get; set; }
+        public List<Models.Workout> Workouts;
+        public List<string> Motivation { get; set; } = new List<string>
+        { "Every workout is progress towards a stronger and healthier you. Keep pushing yourself to reach your goals!"
+        , "Sweat now, shine later. Remember that every drop of sweat is bringing you closer to your fitness goals."
+        , "Don't stop when it hurts, stop when you're done. Stay focused and determined to finish your workout strong."
+        , "Your body can stand almost anything. It's your mind you have to convince. Believe in yourself and your ability to push through any challenge."
+        , "You are stronger than you think. Trust in your abilities and keep pushing yourself to new heights with every workout."};
 
-        public IndexModel(AppDbContext database)
+        private readonly AppDbContext database;
+        private readonly AccessControl accessControl;
+        public IndexModel(AppDbContext database, AccessControl accessControl)
         {
             this.database = database;
+            this.accessControl = accessControl;
         }
 
         public void OnGet()
         {
+            Random randomMotivatíon = new Random();
+            int randomIndex = randomMotivatíon.Next(Motivation.Count);
+            RandomMotivation = Motivation[randomIndex];
 
+            var allWorkouts = database.Workouts.Include(x => x.Owner).Where(x => x.AccessLevel == AccessLevel.Everyone && x.Owner.Id != accessControl.LoggedInAccountID).ToList();
+
+            Random randomWorkout = new Random();
+            var shuffledWorkouts = allWorkouts.OrderBy(x => randomWorkout.Next()).ToList();
+
+            Workouts = shuffledWorkouts.Take(5).Select(x => x).ToList();
         }
     }
 }
