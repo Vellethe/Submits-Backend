c2be423729e4f23745031991ec9119a974ab536a
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-05-12 14:35:15 +0200

    workout page now able to find the most used mucleGroup

--- TrainingProject/Models/Workout.cs
+++ TrainingProject/Models/Workout.cs
@@ -1,4 +1,6 @@
-﻿namespace TrainingProject.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrainingProject.Models
 {
     public enum AccessLevel
     {
@@ -14,6 +16,30 @@
         public AccessLevel AccessLevel { get; set; }
         public Account Owner { get; set; }
         public virtual List<WorkoutExecise> WorkoutExecises { get; set; }
+
+        public MuscleGroup MostCommonMucleGroup
+        {
+            get
+            {
+                var temp = new Dictionary<MuscleGroup, int>();
+                foreach(var exercise in WorkoutExecises )
+                {
+                    var mucleGroupToFind = exercise.Exercise.MuscleGroup;
+
+                    if (temp.ContainsKey(mucleGroupToFind)){
+                        temp[mucleGroupToFind]++;
+                    }
+                    else
+                    {
+                        temp[mucleGroupToFind] = 1;
+                    }
+                }
+
+                return temp.Select(x => new { x.Key, x.Value }).OrderByDescending(x => x.Value).First().Key;
+            }
+            
+        }
+        
 
     }
 }
