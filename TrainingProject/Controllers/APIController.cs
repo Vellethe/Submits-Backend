223e41194b56940b78c001c34d117cb604f751c0
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-05-12 11:57:04 +0200

    Fake-fetch template and start of controllers

--- TrainingProject/Controllers/APIController.cs
+++ TrainingProject/Controllers/APIController.cs
@@ -1,19 +1,53 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 using TrainingProject.Data;
+using TrainingProject.Models;
 
 namespace TrainingProject.Controllers
 {
-    [Route("/api")]
     [ApiController]
+    [Route("/api")]
     public class APIController : ControllerBase
     {
-        private readonly AppDbContext database;
+        private readonly AppDbContext _database;
+        private readonly List<Account> _tempaccounts = new List<Account>();
 
         public APIController(AppDbContext database)
         {
-            this.database = database;
+            _database = database;
+        }
+
+        [HttpPost]
+        public ActionResult addAccount(Account account)
+        {
+            _tempaccounts.Add(account);
+            return CreatedAtAction(nameof(GetAccount), new { id = account.Id }, account);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetAccount(int id)
+        {
+            var account = _tempaccounts.FirstOrDefault(a => a.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(account);
+        }
+
+        [HttpGet("details")]
+        public ActionResult GetDetails()
+        {
+            var account = _database.Accounts.Select(a => new
+            {
+                a.Height,
+                a.CurentWeight,
+                a.TargetWeight
+            }).ToList();
+            return Ok(account);
         }
     }
 }
