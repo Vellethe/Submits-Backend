14841a965ed183693d3457569991d936331e2038
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-05-14 11:36:42 +0200

    Some additional quotes

--- TrainingProject/Pages/Index.cshtml.cs
+++ TrainingProject/Pages/Index.cshtml.cs
@@ -15,7 +15,17 @@ namespace TrainingProject.Pages
         , "Sweat now, shine later. Remember that every drop of sweat is bringing you closer to your fitness goals."
         , "Don't stop when it hurts, stop when you're done. Stay focused and determined to finish your workout strong."
         , "Your body can stand almost anything. It's your mind you have to convince. Believe in yourself and your ability to push through any challenge."
-        , "You are stronger than you think. Trust in your abilities and keep pushing yourself to new heights with every workout."};
+        , "You are stronger than you think. Trust in your abilities and keep pushing yourself to new heights with every workout."
+		, "The only bad workout is the one that didn't happen."
+        , "The pain you feel today will be the strength you feel tomorrow."
+        , "Don't wait for inspiration, be the inspiration."
+        , "Sweat is fat crying."
+        , "A one-hour workout is only 4% of your day. No excuses."
+        , "The body achieves what the mind believes."
+        , "The hardest lift of all is lifting your butt off the couch."
+        , "It's not about perfect, it's about effort. And when you bring that effort every single day, that's where transformation happens."
+        , "Fitness is not about being better than someone else, it's about being better than you used to be."
+        , "You don't have to be great to start, but you have to start to be great."};
 
         private readonly AppDbContext database;
         private readonly AccessControl accessControl;
