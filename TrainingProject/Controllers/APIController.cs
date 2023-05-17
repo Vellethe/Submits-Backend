c6a77a8f33c5a0b98af64502142231b9b4072be4
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-05-17 11:50:29 +0200

    Api and About page

--- TrainingProject/Controllers/APIController.cs
+++ TrainingProject/Controllers/APIController.cs
@@ -18,5 +18,28 @@ namespace TrainingProject.Controllers
         {
             return "hello world";
         }
+
+        [HttpGet]
+        [Authorize("AllowAnonymousApi")]
+        public List<string> OnGetPizza(int calories)
+        {
+            //Time(in minutes) = Calories to burn / Calories burned per minute
+            //All values (3, 9, 15) are based on an average person.
+
+            int walkTime = calories / 3;
+            int weightTime = calories / 9;
+            int runTime = calories / 15;
+
+            string walk = "Walking: " + walkTime + " minutes";
+            string weight = "Weight training: " + weightTime + " minutes";
+            string run = "Run: " + runTime + " minutes";
+
+            List<string> times = new List<string>();
+            times.Add(walk);
+            times.Add(weight);
+            times.Add(run);
+
+            return times;
+        }
     }
 }
