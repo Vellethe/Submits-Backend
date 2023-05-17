9d4c553f91cff50ec6c934ffc003114515ef5c79
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-05-17 18:32:05 +0200

    migrations + instruction

---
+++ TrainingProject/Migrations/20230517162953_instructionExisting.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingProject.Migrations
+{
+    public partial class instructionExisting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Instruction",
+                table: "Exercises",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Instruction",
+                table: "Exercises");
+        }
+    }
+}
