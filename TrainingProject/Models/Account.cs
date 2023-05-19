4f5484d49e945ed14999a3cb3d57fa7d43839e60
1256ID <112385041+1256ID@users.noreply.github.com>
2023-05-19 13:12:28 +0200

    Adding code for methods/graph.

--- TrainingProject/Models/Account.cs
+++ TrainingProject/Models/Account.cs
@@ -5,84 +5,13 @@ namespace TrainingProject.Models
 {
     public class Account
     {
-        public int Id { get; set; }
+        public int Id { get; set; }       
         public string OpenIDIssuer { get; set; }
         public string OpenIDSubject { get; set; }
         public string Name { get; set; }
-        public int Age { get; set; }
-        public int Height { get; set; }
-        public int CurrentWeight {get; set; }
         public bool IsMale { get; set; }
-        public int TargetWeight { get; set; }
-        public string Goal { get; set; } = "None" ;
+        public int Age { get; set; }
         public virtual List<Workout> Workouts { get; set; }
-        public int DayCount(DateTime curentDate, DateTime targetDate)
-        {
-            TimeSpan timeSpan = targetDate - curentDate;
-            int numberOfDays = timeSpan.Days;
-
-            return numberOfDays;
-        }
-
-        public int CalorieCut(Account account)
-        {
-            int curentWeight = account.CurrentWeight;
-            int targetWeight = account.TargetWeight;
-            int caloriesTotal = (curentWeight - targetWeight) * 7700;
-            int numberOfDays = caloriesTotal / 600;
-
-            if (account.Goal == "Lose Weight")
-            {
-                return caloriesTotal / numberOfDays;
-            }
-            else if(account.Goal == "Maintain")
-            {
-                return 0;
-            }
-            else
-            {
-
-            }
-            return 0;
-        }
-        public (string FinishedBMR, string FinishedDate) CalorieCalculator(Account account)
-        {          
-            double heightInMeters = account.Height / 100.0;
-            double bmi = account.TargetWeight / (heightInMeters * heightInMeters);
-
-            int calorieCut = this.CalorieCut(account);
-
-            if(bmi < 19.5)
-            {
-                return ("0","");
-            }
-            else
-            {
-                double bmr = CalculateBMR(account);
-
-                double finishedBMR = Math.Round(bmr) - calorieCut;
-                int caloriesTotal = (account.CurrentWeight - account.TargetWeight) * 7700;
-                int numberOfDays = caloriesTotal / 600;
-                DateTime finishedDate = DateTime.Now.AddDays(numberOfDays);
-
-                return (finishedBMR.ToString(), finishedDate.ToString("yyyy/MM/dd"));
-            }
-        }
-
-        public double CalculateBMR(Account account)
-        {
-            if (account.IsMale)
-            {
-                return ((10 * account.CurrentWeight) + (6.25 * account.Height) - (5 * account.Age) + 5) * 1.55;
-            }
-
-            else
-            {
-                return ((10 * account.CurrentWeight) + (6.25 * account.Height) - (5 * account.Age) - 161) * 1.55;
-            }
-        }
-
-      
-
+        public virtual List<AccountData> AccountData { get; set; }
     }
 }
--- TrainingProject/Pages/Graph/Index.cshtml.cs
+++ TrainingProject/Pages/Graph/Index.cshtml.cs
@@ -1,12 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TrainingProject.Data;
+using TrainingProject.Models;
 
 namespace TrainingProject.Pages.Graph
 {
     public class IndexModel : PageModel
     {
+        public int loggedInID { get; set; }
+        public (string FinishedBMR, string FinishedDate) calorieCount { get; set; }
+        public Account account { get; set; }
+        public Account user { get; set; }
+        public AccountData accountData { get; set; }
+        public AccountData userData { get; set; }
+
+        private readonly AppDbContext context;
+        public IndexModel(AppDbContext context, AccessControl access)
+        {
+            this.context = context;
+            loggedInID = access.LoggedInAccountID;
+            account = new Account();
+            user = context.Accounts.First(u => u.Id == loggedInID);
+            accountData = new AccountData();
+
+        }
         public void OnGet()
         {
+            calorieCount = accountData.CalorieCalculator(user, userData);
+        }
+
+        public IActionResult OnPost(int age, int height, int weight, bool gender, string goal, int targetWeight)
+        {
+            var user = context.Accounts.First(u => u.Id == loggedInID);
+
+            user.Age = age;
+            userData.Height = height;
+            userData.CurrentWeight = weight;
+            user.IsMale = gender;
+            userData.TargetWeight = targetWeight;
+            userData.Goal = goal;
+
+            context.SaveChanges();
+
+            calorieCount = accountData.CalorieCalculator(user, userData);
+
+            return Page();
         }
     }
 }
