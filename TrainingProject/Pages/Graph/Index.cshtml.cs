14a2f8fa3374cf2d742deccf6279fdd3e3f67374
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-05-19 09:43:44 +0200

    Graph page + button + adding my changes to Axels

---
+++ TrainingProject/Pages/Graph/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TrainingProject.Pages.Graph
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
