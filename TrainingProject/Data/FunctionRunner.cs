3e13257a0784e60c3e740a939365461fd7f2c3de
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-10-11 10:42:52 +0200

    funtions and funtion runner

---
+++ TrainingProject/Data/FunctionRunner.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Runtime.InteropServices;
+using WorkoutFuntions;
+
+namespace TrainingProject.Data
+{
+    public class FunctionRunner
+    {
+        public static async Task<string> HelloWorld(string name)
+        {
+            var uri = new Uri("https://workoutfunction.azurewebsites.net/api/HelloWorld?");
+            
+            var client = new HttpClient();
+
+
+            var response = await client.GetStringAsync(uri+$"name={name}");
+            return response;
+        }
+    }
+}
