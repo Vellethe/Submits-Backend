8e54ad4e1536f024462a0f1ee6a2204526fcef62
1256ID <112385041+1256ID@users.noreply.github.com>
2023-10-13 09:53:39 +0200

    Revert "--"
    This reverts commit 97a66e1b96ff7b850a8fdb62ecddc7312341cea2.

--- TrainingProject/Pages/MyPage/Graph/Result.cshtml.cs
+++ TrainingProject/Pages/MyPage/Graph/Result.cshtml.cs
@@ -3,16 +3,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using TrainingProject.Data;
 using TrainingProject.Models;
 
-namespace TrainingProject.Pages.Graph
+namespace TrainingProject.Pages.MyPage.Result
 {
     public class IndexModel : PageModel
     {
         public int LoggedInId { get; set; }
-        public (string FinishedBMR, string FinishedDate) CalorieCount { get; set; }
         public Account Account { get; set; }
         public Account User { get; set; }
         public AccountData AccountData { get; set; }
         public AccountData UserData { get; set; }
+        public (string FinishedBMR, string FinishedDate) CalorieCount { get; set; }
+        public decimal WeightDifference { get; set; }
+        public (string TargetBMI, string CurrentBMI) BMI { get; set; }
+
+        public (double x, double y)[] Coordinates { get; set; }
+        public (double xCoordinate, double yCoordinate) CurrentCoordinate { get; set; }
+        public string[] XValues { get; set; }
+        public double[] WeightPerDataPoint { get; set; }
 
         private readonly AppDbContext context;
         public IndexModel(AppDbContext context, AccessControl access)
@@ -27,21 +34,20 @@ namespace TrainingProject.Pages.Graph
         }
         public void OnGet()
         {
-                    
-                if (UserData!.Goal == "Maintain" && User.CurrentWeight != UserData.TargetWeight)
-                {
-                    UserData.TargetWeight = User.CurrentWeight;
-                }
-
-                context.SaveChanges();
-            
-            CalorieCount = UserData.CalorieCalculator(User, UserData);
+            XValues = UserData.GetXAxisValues(UserData);
+            Coordinates = UserData.GetCoordinates(User, UserData);
+            WeightPerDataPoint = UserData.GetWeightPerCoordinate(User, UserData);
+            decimal weightLeft = (decimal)UserData.WeightDifference(User, UserData, true);
+            CalorieCount = UserData.CalorieCalculator(User, UserData, UserData.Goal, UserData.TargetWeight);
+            WeightDifference = Math.Round(weightLeft);
+            BMI = ((UserData.BMICalculator(User, UserData, false).ToString("0.00")), (UserData.BMICalculator(User, UserData, true).ToString("0.00")));
+            //BMI = (UserData.ConvertToDecimal(UserData.BMICalculator(User, UserData, false)), UserData.ConvertToDecimal(UserData.BMICalculator(User, UserData, true)));
         }
 
-        public IActionResult OnPost(int age, int height, int weight, bool gender, string goal, int targetWeight)
+        public IActionResult OnPost()
         {
 
-            CalorieCount = AccountData.CalorieCalculator(User, UserData);
+            CalorieCount = AccountData.CalorieCalculator(User, UserData, UserData.Goal, UserData.TargetWeight);
 
             return Page();
         }
